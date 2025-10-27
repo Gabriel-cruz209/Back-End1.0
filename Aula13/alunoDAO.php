33ea30976766df4b759f17f6ebc6a36a1c3ff9f5
Gabriel-cruz209 <gc572640@gmail.com>
2025-10-27 15:50:50 -0300

    Atualizado

--- Aula13/alunoDAO.php
+++ Aula13/alunoDAO.php
@@ -2,9 +2,49 @@
 
 class AlunoDAO{ //Classe DAO(Data Access Object) para manipulação das funcões de CRUD (create, read, update e delete)
     private $alunos = [];// Array $alunos para armazenamento dos objetos a serem manipulados,antes de serem enviados ao banco de dados.Foi criado vazio inicialmente
+    private $arquivo = "alunos.json"; //Cria o arquivo json para que os dados sejam armazenados
+
+    // Construtor AlunoDAO --> carrega os dados do arquivo json ao iniciar a aplicação
+
+    public function __construct(){
+        if(file_exists($this->arquivo)){
+            //Lê o conteudo do arquivo caso ele já exista
+            $conteudo = file_get_contents($this->arquivo);// Atribui as informaçoes do arquivo existentes á variavel $conteudo
+
+            $dados = json_decode($conteudo,true); //Converter um json em um array associativo
+
+            if($dados){ // Verifica se o array é nulo ou falso,caso seja valido e contenha conteudo, ele prossgue para a logica detro do if
+                foreach($dados as $id => $info){ // Percorre o array $dados relacionando a chave e valor
+                    $this->alunos[$id] = new Aluno ( // Cria um novo objeto já com as chaves e valores associados
+                        $info['id'],
+                        $info['nome'],
+                        $info['curso'],
+                    );
+                }
+            }
+        }
+    }
+
+    //Metodo Auxiliar -> para salvar o array $alunos no arquivo JSON
+
+    private function salvarEmArquivos(){
+        $dados = [];
+
+        // Transform os objetos em arrays convencionais
+        foreach($this->alunos as $id => $aluno){
+            $dados[$id] = [
+                'id' => $aluno -> getId(),
+                'nome' => $aluno -> getNome(),
+                'curso' =>$aluno -> getCurso(),
+            ];
+        }
+        // Converte para JSON formatado e grava os arquivos
+        file_put_contents($this->arquivo, json_encode($dados, JSON_PRETTY_PRINT));
+    }
 
     public function criarAlunos(Aluno $aluno){ // Metodo para criar um objeto no array alunos -> Create
         $this->alunos[$aluno->getId()] = $aluno;
+        $this->salvarEmArquivos();
     }
     
     public function lerAlunos(){ //Metodo para leros dados d um objeto criado --> read
@@ -16,10 +56,12 @@ class AlunoDAO{ //Classe DAO(Data Access Object) para manipulação das funcões
             $this-> alunos[$id] -> setNome($novoNome);
             $this-> alunos[$id] -> setCurso($novoCurso);
         }
+        $this->salvarEmArquivos();
     }
 
     public function excluirAlunos($id){ //Metodo para excluir um objeto ja criado --> delete
         unset($this->alunos[$id]);
+        $this->salvarEmArquivos();
     }
 }
 
--- Aula13/index.php
+++ Aula13/index.php
@@ -1,7 +1,9 @@
+
 <?php 
 
 require_once "crud.php";
 require_once "alunoDAO.php";
+
 
 $dao = new AlunoDAO();// Objeto da classe AlunoDAO para testar os metodos CRUD
 
@@ -9,6 +11,12 @@ $dao = new AlunoDAO();// Objeto da classe AlunoDAO para testar os metodos CRUD
 $dao->  criarAlunos( new Aluno(1,"Josias","Panificação") );
 $dao->  criarAlunos(new Aluno(2,"Victor Hugo","Manicure") );
 $dao->  criarAlunos(new Aluno(3,"Beatriz","Eletricista") );
+$dao->  criarAlunos(new Aluno(4,"Aurora","Arquitetura") );
+$dao->  criarAlunos(new Aluno(5,"Oliver","Gestão") );
+$dao->  criarAlunos(new Aluno(6,"Amanda","Luta") );
+$dao->  criarAlunos(new Aluno(7,"Geysa","Engenharia") );
+$dao->  criarAlunos(new Aluno(8,"Joab","Eletrica") );
+$dao->  criarAlunos(new Aluno(9,"Miguel","Streamer") );
 
 //READ
 echo"Listagem Inicial\n";
@@ -18,6 +26,12 @@ foreach($dao->lerAlunos() as $aluno){
 
 //UPDATE
 $dao->atualizarAlunos(1,"Jozias","Tecnico em Borracharia");
+$dao->atualizarAlunos(7,"Clotilde","Engenharia");
+$dao->atualizarAlunos(8,"Joana","Eletrica");
+$dao->atualizarAlunos(9,"Miguel","Designer");
+$dao->atualizarAlunos(6,"Amanda","Logistica");
+$dao->atualizarAlunos(5,"Oliver","Eletrica");
+
 echo"\n Após atualização: ";
 foreach($dao->lerAlunos() as $aluno) {
     echo"{$aluno->getId()} - {$aluno->getNome()} - {$aluno->getCurso()}\n";
@@ -25,6 +39,8 @@ foreach($dao->lerAlunos() as $aluno) {
 
 // DELETE
 $dao->excluirAlunos(1);
+$dao->excluirAlunos(7);
+$dao->excluirAlunos(4);
 echo"\n Após exclusão: ";
 foreach($dao->lerAlunos() as $aluno){
     echo"{$aluno->getId()} - {$aluno->getNome()} - {$aluno->getCurso()}\n";
