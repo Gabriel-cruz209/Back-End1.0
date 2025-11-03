27d24a39033b4f1ad839726771c87c721fa7ae28
Gabriel-cruz209 <gc572640@gmail.com>
2025-11-03 16:44:18 -0300

    Atualizado 03/11/2025

---
+++ Aula15/Controller/bebidaController.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once __DIR__ . "/../Model/bebidaDAO.php";
+require_once __DIR__ . "/../Model/bebida.php";
+
+class BebidaController{
+    private $dao;
+
+    //Constructor: cria um objeto DAO(responsavel por salvar e carregar)
+    public function __construct() {
+        $this->dao = new bebidaDAO();
+    }
+
+    //Lista todas as bebidas
+    public function ler(){
+        return $this->dao->lerBebidas();
+    }
+
+    //cadastrar novas bebidas
+    public function criar($nome, $categoria, $volume, $valor, $qtde){
+        //Gera ID automaticamente com base no timeskip(Exemplo simples)
+        $id = time();
+        $bebida = new Bebida($id,$nome, $categoria, $volume, $valor, );
+        $this->dao->criarBebidas($bebida);
+
+    }
+
+    //Atualiza bebida exitente
+    public function atualizar($nome, $valor, $qtde){
+        $this->dao->atualizarBebidas($nome, $valor, $qtde);
+    }
+
+    //Excluir bebida
+    public function deletar($nome){
+        $this->dao->excluirBebibas($nome);
+    }
+}
+
+?>
---
+++ Aula15/Model/bebidaDAO.php
@@ -0,0 +1,71 @@
+<?php
+
+class BebidaDAO{
+    private $bebidasArray = [];
+    private $arquivosJson = 'bebidas.json';
+
+    public function __construct(){
+        if(file_exists("bebidas.json")) {
+            $conteudoArquivo = file_get_contents($this->arquivosJson);
+
+            $dadosArquivoEmArray = json_decode($conteudoArquivo, true);
+
+            if($dadosArquivoEmArray){
+                foreach($dadosArquivoEmArray as $nome => $info) {
+                    $this->bebidasArray[$nome] = new Bebida(
+                        $info['nome'],
+                        $info['categoria'],
+                        $info['volume'],
+                        $info['valor'],
+                        $info['qtde'],
+                    );
+                }
+            }
+        }
+    }
+
+    private function salvarArquivos(){
+        $dadosParaSalvar = [];
+
+        foreach($this-> bebidasArray as $nome => $bebida) {
+            $dadosParaSalvar[$nome] = [
+                'nome' => $bebida -> getNome(),
+                'categoria' => $bebida -> getCategoria(),
+                'volume' => $bebida -> getVolume(),
+                'valor' => $bebida -> getValor(),
+                'qtde' => $bebida -> getQtde(),
+            ];
+        }
+        file_put_contents($this->arquivosJson, json_encode($dadosParaSalvar, JSON_PRETTY_PRINT));
+    }
+
+    //CREATE
+    public function criarBebidas(Bebida $bebida){
+        $this->bebidasArray[$bebida->getNome()] = $bebida;
+        $this->salvarArquivos();
+    }
+
+    //UPDATE
+    public function atualizarBebidas($nome, $novoValor, $novoQtde){
+        if(isset($bebidasArray[$nome])){
+            $this->bebidasArray[$nome]->setValor($novoValor);
+            $this->bebidasArray[$nome]->setQtde($novoQtde);
+        }
+        $this->salvarArquivos();
+    }
+
+    //READ
+    public function lerBebidas(){
+        return $this->bebidasArray;
+    }
+
+    //DELETE
+    public function excluirBebibas($nome){
+        unset($this->
+        bebidasArray[$nome]);
+        $this->salvarArquivos();
+    }
+}
+
+
+?>
